80a18826859cff3a7b84bfb198e2bb1ec6e18ed4
John Oxley <john.oxley@gmail.com>
2016-01-11 12:28:11 +0000

    v0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='fptest',
-    version='0.1.0',
+    version='0.1.1',
     description='A TIBCO Fulfilment Provisioning test helper',
     long_description=long_description,
     packages=['fptest'],
